6b0ccee32bb5dd21b779cba1d92fd11332fa4531
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-03 23:06:18 +0300

    Added ViewComponent for Testimonials area

--- Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.TestimonialDtos;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
@@ -19,8 +20,8 @@ namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
             if(responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                //var values = JsonConvert.DeserializeObject<>(jsonData);
-                //return View(values);
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
