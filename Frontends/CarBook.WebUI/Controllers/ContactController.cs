ab8b0b7de5831beca08975a48e7a4fcd5a0019db
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-04 02:19:48 +0300

    Added ContactController and ViewComponent for Address area

---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7160/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
