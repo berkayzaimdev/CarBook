d197621de04d14f4a7532d3b3f4edd4091c31366
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-03 22:48:37 +0300

    Added CQRS classes of Testimonial entity

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Core.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class GetTestimonialQueryHandler : IRequestHandler<GetTestimonialQuery, List<GetTestimonialQueryResult>>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetTestimonialQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetTestimonialQueryResult>> Handle(GetTestimonialQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(value => new GetTestimonialQueryResult 
+            {
+                TestimonialID = value.TestimonialID,
+                Name = value.Name,
+                Title = value.Title,
+                Comment = value.Comment,
+                ImageUrl = value.ImageUrl
+            }).ToList();
+        }
+    }
+}
