2f24d207503c38d2e40ecc766f92d34da27dab0b
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-07 13:55:36 +0300

    Added CQRS classes of Blog entity

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/BlogHandlers/GetBlogQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Application.Features.Mediator.Queries.BlogQueries;
+using CarBook.Application.Features.Mediator.Results.BlogResults;
+using CarBook.Core.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.BlogHandlers
+{
+    public class GetBlogQueryHandler : IRequestHandler<GetBlogQuery, List<GetBlogQueryResult>>
+    {
+        private readonly IRepository<Blog> _repository;
+
+        public GetBlogQueryHandler(IRepository<Blog> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetBlogQueryResult>> Handle(GetBlogQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(value => new GetBlogQueryResult 
+            {
+                BlogID = value.BlogID,
+                AuthorID = value.AuthorID,
+                CategoryID = value.CategoryID,
+                Title = value.Title,
+                CreatedDate = value.CreatedDate,
+                CoverImageUrl = value.CoverImageUrl
+            }).ToList();
+        }
+    }
+}
