d0df6c6bcdf37505dc7919f9d4bacb7150a5e10c
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-07 15:07:19 +0300

    Added ViewComponent for Statistics area

---
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultStatisticsComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
