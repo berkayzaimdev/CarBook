e4ea0883b1b9fa982791b4df151e760a66840fb4
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-07 12:04:43 +0300

    Modified ServiceController's Index View into a ViewComponent

--- Frontends/CarBook.WebUI/Controllers/ServiceController.cs
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -7,23 +7,8 @@ namespace CarBook.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public ServiceController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7160/api/Services");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
-                return View(values);
-            }
+        public IActionResult Index()
+        { 
             return View();
         }
     }
