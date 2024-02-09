c1caebcc84111a8d3947264688aaf39230549936
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-09 09:20:23 +0300

    Added some ViewComponents for BlogController's Detail View

--- Frontends/CarBook.WebUI/Controllers/BlogController.cs
+++ Frontends/CarBook.WebUI/Controllers/BlogController.cs
@@ -28,5 +28,12 @@ namespace CarBook.WebUI.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> BlogDetail(int id)
+        {
+            ViewBag.v1 = "Bloglar";
+            ViewBag.v2 = "Blog Detayı ve Yorumlar";
+            return View();
+        }
     }
 }
---
+++ Frontends/CarBook.WebUI/ViewComponents/BlogViewComponents/_BlogDetailSideBarSearchBoxComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using CarBook.Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogDetailSideBarSearchBoxComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
