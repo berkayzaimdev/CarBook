bb527734ba3c65535cd70bdf96dc219cc31d26c9
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-11 23:46:25 +0300

    Added CQRS classes of TagCloud entity

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TagCloudHandlers/UpdateTagCloudCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CarBook.Application.Features.Mediator.Commands.TagCloudCommands;
+using CarBook.Core.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TagCloudHandlers
+{
+    public class UpdateTagCloudCommandHandler : IRequestHandler<UpdateTagCloudCommand>
+    {
+        private readonly IRepository<TagCloud> _repository;
+
+        public UpdateTagCloudCommandHandler(IRepository<TagCloud> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateTagCloudCommand request, CancellationToken cancellationToken)
+        {
+            var value = await _repository.GetByIdAsync(request.TagCloudID);
+            value.BlogID = request.BlogID;
+            value.Title = request.Title;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
