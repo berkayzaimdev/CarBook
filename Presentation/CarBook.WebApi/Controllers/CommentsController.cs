f91fb631fa1fe60d90e4bb89d6619df287f1b0b1
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-13 00:12:59 +0300

    Added Repository class for Comment entity

---
+++ Presentation/CarBook.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,55 @@
+﻿using CarBook.Application.Features.RepositoryPattern;
+using CarBook.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly IGenericRepository<Comment> _repository;
+
+        public CommentsController(IGenericRepository<Comment> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList() 
+        {
+            var values = _repository.GetAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(Comment comment)
+        {
+            _repository.Create(comment);
+            return Ok("Comment başarıyla oluşturuldu");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveComment(int id)
+        {
+            var value = _repository.GetById(id);
+            _repository.Remove(value);
+            return Ok("Comment başarıyla silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(Comment comment)
+        {
+            _repository.Update(comment);
+            return Ok("Comment başarıyla güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _repository.GetById(id);
+            return Ok(value);
+        }
+    }
+}
