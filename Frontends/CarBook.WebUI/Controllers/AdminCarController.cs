4bd877d8fc71e33224091620168288f42cf76283
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-17 15:38:22 +0300

    Added RemoveCar method to AdminCarController

--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -60,5 +60,16 @@ namespace CarBook.WebUI.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> RemoveCar(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7160/api/Cars/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
