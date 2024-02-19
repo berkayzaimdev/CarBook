49700cffa8c4e4383d8f58c9b4224a273760252b
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-19 11:31:02 +0300

    Added AdminCommentController and its Index IActionResult

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,34 @@
+﻿using CarBook.Dto.BlogDtos;
+using CarBook.Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Route("Admin/AdminComment")]
+    [Area("Admin")]
+    public class AdminCommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            ViewBag.v = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7160/api/Comments/GetCommentsByBlogId?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
