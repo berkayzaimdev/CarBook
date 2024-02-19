82e3a66967d624dba84e3dd446a75f4ea4e79cfe
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-19 15:36:23 +0300

    Consumed all methods of the API into the AdminStatisticsController and modified its Index View

--- Infrastructure/CarBook.Persistence/Repositories/StatisticsRepositories/StatisticsRepository.cs
+++ Infrastructure/CarBook.Persistence/Repositories/StatisticsRepositories/StatisticsRepository.cs
@@ -20,12 +20,29 @@ namespace CarBook.Persistence.Repositories.StatisticsRepositories
 
         public async Task<string> GetBlogTitleByMaxBlogCommentAsync()
         {
-            throw new NotImplementedException();
+            //Select Top(1) BlogId,Count(*) as 'Sayi' From Comments Group By BlogID Order By Sayi Desc 
+            var values = await _context.Comments.GroupBy(x => x.BlogID).
+                              Select(y => new
+                              {
+                                  BlogID = y.Key,
+                                  Count = y.Count()
+                              }).OrderByDescending(z => z.Count).Take(1).FirstOrDefaultAsync();
+            string blogName = await _context.Blogs.Where(x => x.BlogID == values.BlogID).Select(y => y.Title).FirstOrDefaultAsync();
+            return blogName;
         }
 
         public async Task<string> GetBrandNameByMaxCarAsync()
         {
-            throw new NotImplementedException();
+            //Select Top(1) BrandId,Count(*) as 'ToplamArac' From Cars Group By Brands.Name  order By ToplamArac Desc
+
+            var values = await _context.Cars.GroupBy(x => x.BrandID).
+                             Select(y => new
+                             {
+                                 BrandID = y.Key,
+                                 Count = y.Count()
+                             }).OrderByDescending(z => z.Count).Take(1).FirstOrDefaultAsync();
+            string brandName = await _context.Brands.Where(x => x.BrandID == values.BrandID).Select(y => y.Name).FirstOrDefaultAsync();
+            return brandName;
         }
 
         public async Task<int> GetAuthorCountAsync()
@@ -69,12 +86,21 @@ namespace CarBook.Persistence.Repositories.StatisticsRepositories
 
         public async Task<string> GetCarBrandAndModelByRentPriceDailyMaxAsync()
         {
-            throw new NotImplementedException();
+            //Select * From CarPricings where Amount=(Select Max(Amount) From CarPricings where PricingID=3)
+            int pricingID = await _context.Pricings.Where(x => x.Name == "Günlük").Select(y => y.PricingID).FirstOrDefaultAsync();
+            decimal amount = await _context.CarPricings.Where(y => y.PricingID == pricingID).MaxAsync(x => x.Amount);
+            int carId = await _context.CarPricings.Where(x => x.Amount == amount).Select(y => y.CarID).FirstOrDefaultAsync();
+            string brandModel = await _context.Cars.Where(x => x.CarID == carId).Include(y => y.Brand).Select(z => z.Brand.Name + " " + z.Model).FirstOrDefaultAsync();
+            return brandModel;
         }
 
         public async Task<string> GetCarBrandAndModelByRentPriceDailyMinAsync()
         {
-            throw new NotImplementedException();
+            int pricingID = await _context.Pricings.Where(x => x.Name == "Günlük").Select(y => y.PricingID).FirstOrDefaultAsync();
+            decimal amount = await _context.CarPricings.Where(y => y.PricingID == pricingID).MinAsync(x => x.Amount);
+            int carId = await _context.CarPricings.Where(x => x.Amount == amount).Select(y => y.CarID).FirstOrDefaultAsync();
+            string brandModel = await _context.Cars.Where(x => x.CarID == carId).Include(y => y.Brand).Select(z => z.Brand.Name + " " + z.Model).FirstOrDefaultAsync();
+            return brandModel;
         }
 
         public async Task<int> GetCarCountAsync()
