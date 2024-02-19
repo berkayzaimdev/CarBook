1367fb63fbedbf40a393fa121a6283c40623cce9
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-19 12:04:42 +0300

    Added AdminPricingController and its Index, Remove and Update IActionResults Added AdminContactController and its Index, Remove and Update IActionResults

---
+++ Frontends/CarBook.WebUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,67 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Areas.Admin.Controllers
+{
+    [Route("Admin/AdminContact")]
+    [Area("Admin")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7160/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateContact")]
+        public async Task<IActionResult> CreateContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateContact")]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7160/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminContact", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("RemoveContact/{id}")]
+        public async Task<IActionResult> RemoveContact(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7160/api/Contacts/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminContact", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
