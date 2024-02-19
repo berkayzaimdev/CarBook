18aae3c97a07b331450cda9b8ff8033df91b7274
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-19 17:34:45 +0300

    Modified the GET and POST methods of DefaultController, Added RentACarListController and its Index method. Transfered the datas between those Controllers

---
+++ Frontends/CarBook.WebUI/Controllers/RentACarListController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class RentACarListController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book_pick_date = TempData["bookpickdate"];
+            var book_off_date = TempData["bookoffdate"];
+            var time_pick = TempData["timepickdate"];
+            var time_off = TempData["timeoff"];
+            var locationID = TempData["LocationID"];
+
+            ViewBag.bookpickdate = book_pick_date;
+            ViewBag.bookoffdate = book_off_date;
+            ViewBag.timepick = time_pick;
+            ViewBag.timeoff = time_off;
+            ViewBag.locationID = locationID;
+            return View();
+        }
+    }
+}
