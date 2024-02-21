49abf19873986d0db8bb1fb3bd8fef329d293e16
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-21 14:39:43 +0300

    Added GetCarPricingWithTimePeriodList to ICarPricingRepository and its CQRS classes, Added CarPricingController and consumed the API for its Index View

--- Infrastructure/CarBook.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
+++ Infrastructure/CarBook.Persistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CarBook.Application.Interfaces.CarPricingInterfaces;
+using CarBook.Application.ViewModels;
 using CarBook.Domain.Entities;
 using CarBook.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
@@ -28,5 +29,37 @@ namespace CarBook.Persistence.Repositories.CarPricingRepositories
                 .Where(x => x.PricingID == 3)   // Sadece günlük fiyatlandırmaya ait verileri çektik
                 .ToListAsync();
         }
-    }
+
+		public async Task<List<CarPricingViewModel>> GetCarPricingWithTimePeriodAsync()
+		{
+			List<CarPricingViewModel> values = new List<CarPricingViewModel>();
+			using (var command = _context.Database.GetDbConnection().CreateCommand())
+			{
+				command.CommandText = "Select * From (Select Model,Name,CoverImageUrl,PricingID,Amount From CarPricings Inner Join Cars On Cars.CarID=CarPricings.CarId Inner Join Brands On Brands.BrandID=Cars.BrandID) As SourceTable Pivot (Sum(Amount) For PricingID In ([2],[3],[4])) as PivotTable;";
+				command.CommandType = System.Data.CommandType.Text;
+				_context.Database.OpenConnection();
+				using (var reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						CarPricingViewModel carPricingViewModel = new CarPricingViewModel()
+						{
+							Brand = reader["Name"].ToString(),
+							Model = reader["Model"].ToString(),
+							CoverImageUrl = reader["CoverImageUrl"].ToString(),
+							Amounts = new List<decimal>
+							{
+								Convert.ToDecimal(reader["2"]),
+								Convert.ToDecimal(reader["3"]),
+								Convert.ToDecimal(reader["4"])
+							}
+						};
+						values.Add(carPricingViewModel);
+					}
+				}
+				_context.Database.CloseConnection();
+				return values;
+			}
+		}
+	}
 }
