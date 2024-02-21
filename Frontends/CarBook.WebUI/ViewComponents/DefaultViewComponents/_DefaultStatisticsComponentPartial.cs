405150f14515f2096fed5260d84c738de1d958f3
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-21 14:52:15 +0300

    Modified _DefaultStatisticsComponentPartial, consumed the API of some Statistics

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,14 +1,66 @@
 ﻿using CarBook.Dto.BannerDtos;
+using CarBook.Dto.StatisticsDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultStatisticsComponentPartial : ViewComponent
-    {
-        public IViewComponentResult Invoke()
-        {
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+
+			#region Araba Sayısı istatistiği
+			var responseMessage = await client.GetAsync("https://localhost:7160/api/Cars/GetCarCount");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+				ViewBag.carCount = values.carCount;
+			}
+			#endregion
+
+			#region Lokasyon Sayısı istatistiği
+			var responseMessage2 = await client.GetAsync("https://localhost:7160/api/Statistics/GetLocationCount");
+			if (responseMessage2.IsSuccessStatusCode)
+			{
+				var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+				var values2 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData2);
+				ViewBag.locationCount = values2.locationCount;
+			}
+            #endregion
+
+            #region Marka Sayısı istatistiği
+            var responseMessage5 = await client.GetAsync("https://localhost:7160/api/Statistics/GetBrandCount");
+            if (responseMessage5.IsSuccessStatusCode)
+            {
+                var jsonData5 = await responseMessage5.Content.ReadAsStringAsync();
+                var values5 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData5);
+                ViewBag.brandCount = values5.brandCount;
+            }
+            #endregion
+
+            #region Elektrikli Araba Sayısı istatistiği
+            var responseMessage14 = await client.GetAsync("https://localhost:7160/api/Statistics/GetCarCountByFuelElectric");
+            if (responseMessage14.IsSuccessStatusCode)
+            {
+                var jsonData14 = await responseMessage14.Content.ReadAsStringAsync();
+                var values14 = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData14);
+                ViewBag.carCountByFuelElectric = values14.carCountByFuelElectric;
+            }
+            #endregion
+
             return View();
-        }
-    }
+		}
+	}
 }
