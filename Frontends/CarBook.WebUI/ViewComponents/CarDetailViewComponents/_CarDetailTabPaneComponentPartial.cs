f5253f4a83a92e3a9e504d85447da52aa36b0d4e
berkayzaimdev <berkayzaimdev@gmail.com>
2024-02-25 15:38:08 +0300

    Added CarDetail View to CarController, Divided the View into some ViewComponents, Added Review entity and its CQRS classes, Added CarDescription and Review repositories and their methods' CQRS classes

---
+++ Frontends/CarBook.WebUI/ViewComponents/CarDetailViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.CarDetailViewComponents
+{
+    public class _CarDetailTabPaneComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+		{
+			return View();
+        }
+    }
+}
